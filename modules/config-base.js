675c65f309534d44864e250250e5bc50fafc727d
Santiago MR <100314893+SantiDEV2@users.noreply.github.com>
2023-11-13 01:45:25 -0600

    Implementacion de la base de datos asi como avances de la pantalla inicial

---
+++ modules/config-base.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-VNuqBZnj6M0JTuumPGeaAQzVlLO34C4",
+  authDomain: "dev5-amerike-desarrolloweb.firebaseapp.com",
+  databaseURL: "https://dev5-amerike-desarrolloweb-default-rtdb.firebaseio.com",
+  projectId: "dev5-amerike-desarrolloweb",
+  storageBucket: "dev5-amerike-desarrolloweb.appspot.com",
+  messagingSenderId: "567292513072",
+  appId: "1:567292513072:web:db707bb77c769a81b116f5",
+};
+
+export const app = initializeApp(firebaseConfig);
+
