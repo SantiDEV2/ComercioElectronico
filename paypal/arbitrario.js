b3635955f5e42876225d73bf8509c517586c4530
Dante <martin.velazquez.luis.alfonso@gmail.com>
2023-11-26 00:27:59 -0600

    carrito logic

--- paypal/arbitrario.js
+++ paypal/arbitrario.js
@@ -1,3 +1,31 @@
 import carrito from "./carrito.js";
 
-carrito.logear("Hola");
+const producto = {
+    "Name": "D&D Nolzur’s Marvelous Unpainted Minis: Nothics",
+    "Price": "115.00",
+    "Description": "Figura de plástico de 3 cm",
+    "Rating": "https://cdn.discordapp.com/attachments/845392998648119356/1177832093456285796/5Stars.png?ex=6573f09e&is=65617b9e&hm=8c399e94553439d0975b3a658b67b0da9400e737fc61b1b9b447271ead911196&",
+    "Img":"https://dmsphylactery.com/wp-content/uploads/2022/12/wzk90526_1.jpg",
+    "Storage":"10",
+    "CreationDate":"15/11/2023",
+    "ID":"007",
+    "Category":"Mini Figure",
+    "SubCategory":"Mini"
+}
+
+const prod = 
+      {
+        "Name": "Crocodile",
+        "Price": "110.00",
+        "Description": "Figura de plástico de 3 cm",
+        "Rating": "https://cdn.discordapp.com/attachments/845392998648119356/1177832122992570448/2Stars.png?ex=6573f0a5&is=65617ba5&hm=f4e64a7ad8ac83b0bc4c75e6aa2e5d14e92985b77f50c154b0713e56de79da2b&",
+        "Img":"https://dmsphylactery.com/wp-content/uploads/2022/12/wzk90051_1.jpg",
+        "Storage":"10",
+        "CreationDate":"15/11/2023",
+        "ID":"006",
+        "Category":"Mini Figure",
+        "SubCategory":"Mini"
+      } 
+
+      carrito.anadir(producto);
+      carrito.remover(producto);
--- paypal/carrito.js
+++ paypal/carrito.js
@@ -1,7 +1,75 @@
 const carrito = {
-    logear : (message) => {
-        console.log(message);
+    anadir : (objecto) => {
+        const currrentCarrito = window.localStorage.getItem("carrito");
+        
+        const carritoObj = JSON.parse(currrentCarrito);
+        const articulos = carritoObj.articulos;
+
+        console.log(articulos);
+
+        let actualizado = false;
+
+        let newArticles = articulos.map((articulo) => {
+            if(articulo.ID === objecto.ID){
+                console.log("Es el mismo objecto");
+                articulo.agregados++;
+                actualizado = true;
+            }
+
+            return articulo;
+        });
+
+        if(!actualizado){
+            //CREAR
+            objecto.agregados = 1;
+            newArticles = [...newArticles, objecto]
+        }
+
+        const newCarrito = {...carritoObj, articulos: newArticles}
+        console.log("new Carrito: ", newCarrito);
+        window.localStorage.setItem("carrito", JSON.stringify(newCarrito))
+    },
+    remover : (objeto) => {
+        const currrentCarrito = window.localStorage.getItem("carrito");
+        
+        const carritoObj = JSON.parse(currrentCarrito);
+        let articulos = carritoObj.articulos;
+
+        if(articulos.lenght <= 0) return;
+
+        let siHay = false;
+        let destroy = false;
+
+        articulos = articulos.map((articulo => {
+            if(articulo.ID === objeto.ID){
+                console.log("Si hay");
+                siHay = true;
+                articulo.agregados--;
+                if(articulo.agregados <= 0){ 
+                    destroy = true; 
+                }
+            }
+
+            return articulo;
+        }));
+
+        if(!siHay){
+            console.log("No hay ese articulo");
+            return;
+        }
+        
+        if(destroy){
+            console.log("Se destruyo")
+            articulos = articulos.filter((articulo) => {
+                return articulo.ID !== objeto.ID;
+            });
+        }
+
+        const newCarrito = {...carritoObj, articulos: articulos}
+        console.log("new Carrito: ", newCarrito);
+        window.localStorage.setItem("carrito", JSON.stringify(newCarrito))
     }
 }
 
+window.localStorage.setItem("carrito", JSON.stringify({articulos : []}));
 export default carrito;
