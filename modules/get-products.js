14dada787d62146c6b5a47f323bba60713a0d8fd
Santiago MR <100314893+SantiDEV2@users.noreply.github.com>
2023-11-25 23:29:15 -0600

    System pages

--- modules/get-products.js
+++ modules/get-products.js
@@ -1,25 +1,43 @@
 import {
-    getDatabase,
-    ref,
-    onValue,
+  getDatabase,
+  ref,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
 
 const db = getDatabase();
-for (let i = 1; i <= 12; i++) {
-    const getData = ref(db, `1/Producto${i}/`);
-    let $products = document.querySelector("#products");
-    onValue(getData, (snapshot) => {
-        const value = snapshot.val();
-        $products.innerHTML += ` 
-            <div class="col-lg-3 col-md-4 col-sm-6 col-12 textColor align-items-center" id="product">
-            <img src=${value.Img} width="200px" height="auto" class="border30";>
-            <h5 style="margin-bottom: 0%;">${value.Name}</h5>
-            <img src="${value.Rating}">
-            <p style="margin-bottom: 0%;">${value.Price}</p>  
-            <p>
-            <button type="button" class="btn mybtnProducts my-3">Añadir al <br> carrito</button>
-            </p>
-            </div>
-        `;
-    });
-}
+
+let $products = document.querySelector("#products");
+let $template = "";
+
+document.addEventListener("click", (e) => {
+  $products.innerHTML = $template;
+  if (e.target.matches("#valueBtn")) {
+    setPage(e.target.value);
+  }
+});
+
+const setPage = (page) => {
+  const data = ref(db, `${page ?? 1}/`);
+  onValue(data, (snapshot) => {
+    const items = snapshot.val();
+    updateWebPage(items);
+  });
+};
+
+setPage();
+
+const updateWebPage = (items) => {
+  Object.values(items).forEach((item) => {
+    $products.innerHTML += ` 
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 textColor align-items-center" id="product">
+        <img src=${item.Img} width="200px" height="auto" class="border30";>
+        <h5 style="margin-bottom: 0%;">${item.Name}</h5>
+        <img src="${item.Rating}">
+        <p style="margin-bottom: 0%;">${item.Price}</p>  
+        <p>
+        <button type="button" class="btn mybtnProducts my-3">Añadir al <br> carrito</button>
+        </p>
+        </div>
+    `;
+  });
+};
