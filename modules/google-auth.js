ca5f00170c3a6f519feafe776ee8fca9be5c8f84
Tobias Gtz <100314893+SantiDEV2@users.noreply.github.com>
2023-11-27 15:51:03 -0600

    Testing

--- modules/google-auth.js
+++ modules/google-auth.js
@@ -6,42 +6,43 @@ import {
   GoogleAuthProvider,
 } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
 
-export function authGoogle(app){
+export function authGoogle(app) {
   const auth = getAuth(),
-  provider = new GoogleAuthProvider(),
-  $GoogleBtn = document.querySelector("#GoogleBtn");
+    provider = new GoogleAuthProvider(),
+    $GoogleBtn = document.querySelector("#GoogleBtn");
 
   let $userText = document.querySelector("#userName"),
-  $accountbtns = document.querySelector("#accountBtns"),
-  $accountInfo = document.querySelector("#accountInfo");
+    $accountbtns = document.querySelector("#accountBtns"),
+    $accountInfo = document.querySelector("#accountInfo"),
+    $accountName;
 
-  onAuthStateChanged(auth,(user) =>{
-    if (user) {
-      console.log(true);
-    } else {
-      console.log(false);
-    }
-  })
-
-  document.addEventListener("click",(e) =>{
-    if(e.target.matches("#GoogleBtn"))
-    {
+  document.addEventListener("click", (e) => {
+    if (e.target.matches("#GoogleBtn")) {
       signInWithPopup(auth, provider)
-      .then((res) => {
-        const credential = GoogleAuthProvider.credentialFromResult(res);
-        const token = credential.accessToken;
-        $userText.innerHTML = `<img src="${res.user.photoURL}" style="width: 30px; height: auto;" class="mx-1">${res.user.displayName}`;
-        $accountInfo.classList.remove("d-none");
-        $accountbtns.classList.add("d-none");
-        console.log(res);
-        console.log(res.user);
-      })
+        .then((res) => {
+          const credential = GoogleAuthProvider.credentialFromResult(res);
+          const token = credential.accessToken;
+          $accountName = res.user.email;
+          console.log($accountName);
+          $userText.innerHTML = `<img src="${res.user.photoURL}" style="width: 30px; height: auto;" class="mx-1" border-30>${res.user.displayName}`;
+          $accountInfo.classList.remove("d-none");
+          $accountbtns.classList.add("d-none");
+          console.log(res.user.photoURL);
+        })
     }
-    if(e.target.matches("#logout"))
-    {
+    if (e.target.matches("#logout")) {
       $accountInfo.classList.add("d-none");
       $accountbtns.classList.remove("d-none")
       signOut(auth);
+    }
+  })
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+
+      console.log(true);
+    } else {
+      console.log(false);
     }
   })
 }
